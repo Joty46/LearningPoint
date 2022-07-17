9f4601f7c47c45c59be1a080cea07473b16d3de5
Joty46 <joty1807046@stud.kuet.ac.bd>
2022-07-17 21:35:27 +0600

    project updated

--- admin.php
+++ admin.php
@@ -10,7 +10,7 @@ $query = mysqli_query($conn, $q);
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Admin</title>
         <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
         <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
